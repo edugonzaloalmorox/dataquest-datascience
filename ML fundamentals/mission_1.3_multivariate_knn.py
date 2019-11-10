9a20f632987f5b0fb010653f30198fae99c7ba39
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2019-11-10 23:02:40 +0100

    Add progress and notes in ML

---
+++ ML fundamentals/mission_1.3_multivariate_knn.py
@@ -0,0 +1,100 @@
+# Removing features 
+
+dc_listings.head()
+
+# remove various variables with MA 
+drop_columns = ['host_response_rate', 'host_acceptance_rate', 'room_type',
+                'host_listings_count', 'city', 
+                'state', 'longitude', 'latitude', 'zipcode']
+
+dc_listings = dc_listings.drop(drop_columns, axis =1)
+
+print(dc_listings)
+
+# Handling missing values
+
+
+dc_listings = dc_listings.drop(['cleaning_fee', 'security_deposit'], axis = 1)
+
+dc_listings = dc_listings.dropna(axis = 0)
+
+# check proportions of NAs in the dataframe
+dc_listings.isna().mean().round(4) * 100
+
+
+# Normalize columns 
+
+normalized_listings = (dc_listings - dc_listings.mean()) / (dc_listings.std())
+
+normalized_listings['price'] = dc_listings['price']
+
+print(normalized_listings.head(3))
+
+# Euclidean distance for multivariate 
+
+from scipy.spatial import distance
+
+first_listing = normalized_listings.iloc[0][['accommodates', 'bathrooms']]
+
+fifth_listing = normalized_listings.iloc[4][['accommodates', 'bathrooms']]
+
+first_fifth_distance = distance.euclidean(first_listing, fifth_listing)
+
+print(first_fifth_distance)
+
+# Fitting a model and making predictions
+
+from sklearn.neighbors import KNeighborsRegressor
+
+
+train_df = normalized_listings.iloc[0:2792]
+test_df = normalized_listings.iloc[2792:]
+train_columns = ['accommodates', 'bathrooms']
+
+knn = KNeighborsRegressor(n_neighbors = 5, algorithm ='brute')
+
+knn.fit(train_df[train_columns], train_df['price'])
+
+predictions = knn.predict(test_df[train_columns])
+
+# Using more features
+
+features = ['accommodates', 'bedrooms', 'bathrooms', 'number_of_reviews']
+from sklearn.neighbors import KNeighborsRegressor
+knn = KNeighborsRegressor(n_neighbors=5, algorithm='brute')
+
+knn.fit(train_df[features], train_df['price'])
+
+four_predictions = knn.predict(test_df[features])
+
+# Evaluate the predictions of the model
+four_mse = mean_squared_error(test_df['price'], four_predictions)
+
+four_rmse = (four_mse)**(1/2)
+
+# Using all the features of the Dataset 
+
+train_df = normalized_listings.iloc[0:2792]
+
+test_df = normalized_listings.iloc[2792:]
+
+# Select features (all but price)
+features = train_df.columns.tolist()
+features.remove('price')
+
+
+from sklearn.neighbors import KNeighborsRegressor
+knn = KNeighborsRegressor(n_neighbors=5, algorithm='brute')
+
+# Fit
+knn.fit(train_df[features], train_df['price'])
+
+all_features_predictions = knn.predict(test_df[features])
+
+all_features_mse = mean_squared_error(test_df['price'], all_features_predictions)
+
+all_features_rmse= (all_features_mse)**(1/2)
+
+print(all_features_mse, all_features_rmse)
+
+
--- Working with data sources/mission4.1_working_with_API.py
+++ Working with data sources/mission4.1_working_with_API.py
@@ -5,3 +5,82 @@ response = requests.get("http://api.open-notify.org/iss-now.json")
 status_code = response.status_code
 
 status_code
+
+# Set up the parameters we want to pass to the API.
+# This is the latitude and longitude of New York City.
+
+parameters = {"lat": 40.71, "lon": -74}
+
+# Make a get request with the parameters.
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+
+# Print the content of the response (the data the server returned)
+print(response.content)
+
+# This gets the same data as the command above
+response = requests.get("http://api.open-notify.org/iss-pass.json?lat=40.71&lon=-74")
+print(response.content)
+
+parameters = {"lat": 37.78, "lon": -122.41}
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+content = response.content
+
+# Make a list of fast food chains.
+best_food_chains = ["Taco Bell", "Shake Shack", "Chipotle"]
+print(type(best_food_chains))
+
+# Import the JSON library.
+import json
+
+# Use json.dumps to convert best_food_chains to a string.
+best_food_chains_string = json.dumps(best_food_chains)
+print(type(best_food_chains_string))
+
+# Convert best_food_chains_string back to a list.
+print(type(json.loads(best_food_chains_string)))
+
+# Make a dictionary
+fast_food_franchise = {
+    "Subway": 24722,
+    "McDonalds": 14098,
+    "Starbucks": 10821,
+    "Pizza Hut": 7600
+}
+
+# We can also dump a dictionary to a string and load it.
+fast_food_franchise_string = json.dumps(fast_food_franchise)
+print(type(fast_food_franchise_string))
+
+fast_food_franchise_2 = json.loads(fast_food_franchise_string)
+
+print(type(fast_food_franchise_2))
+
+# Make the same request we did two screens ago.
+
+parameters = {"lat": 37.78, "lon": -122.41}
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+
+# Get the response data as a Python object.  Verify that it's a dictionary.
+json_data = response.json()
+print(type(json_data))
+print(json_data)
+
+
+
+first_pass_duration = json_data['response'][0]["duration"]
+
+first_pass_duration
+
+# Headers is a dictionary
+print(response.headers)
+
+
+content_type = response.headers['content-type']
+
+# Call the API
+
+response  = requests.get("http://api.open-notify.org/astros.json")
+
+json_data = response.json() # transform into json
+
+in_space_count = json_data["number"]
