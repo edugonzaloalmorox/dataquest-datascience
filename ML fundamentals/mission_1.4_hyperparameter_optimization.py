6a8ab44167b58d4e78fe750ebc88050e274f5315
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2019-11-17 19:54:10 +0100

    hyperparameter and cross validation

--- ML fundamentals/mission_1.4_hyperparameter_optimization.py
+++ ML fundamentals/mission_1.4_hyperparameter_optimization.py
@@ -1,4 +1,4 @@
-# Hyperparameter optimization
+# Hyperparameter optimization ---------------------
 
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
@@ -15,7 +15,7 @@ for hp in hyper_params:
 
 print(mse_values)
 
-# Expanding grid search
+# Expanding grid search --------------------------
 
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
@@ -34,7 +34,7 @@ for hp in hyper_params:
 
 print(mse_values)
 
-# Visualizing hyperparameter values
+# Visualizing hyperparameter values ----------------------
 
 features = ['accommodates', 'bedrooms', 'bathrooms', 'number_of_reviews']
 hyper_params = [x for x in range(1, 21)]
@@ -51,4 +51,71 @@ for hp in hyper_params:
 plt.scatter(hyper_params, mse_values)
 plt.show()
 
-# Varying hyperparameters
+# Varying hyperparameters -----------------------
+
+hyper_params = [x for x in range(1,21)]
+mse_values = list()
+features = train_df.columns.tolist()
+features.remove('price')
+
+
+for hp in hyper_params:
+    knn = KNeighborsRegressor(n_neighbors=hp, algorithm='brute')
+    knn.fit(train_df[features], train_df['price'])
+    predictions = knn.predict(test_df[features])
+    mse = mean_squared_error(test_df['price'], predictions)
+    mse_values.append(mse)
+
+plt.scatter(hyper_params, mse_values)
+plt.show()
+
+# Practice the workflow -----------------
+
+two_features = ['accommodates', 'bathrooms']
+three_features = ['accommodates', 'bathrooms', 'bedrooms']
+hyper_params = [x for x in range(1,21)]
+# Append the first model's MSE values to this list.
+two_mse_values = list()
+
+# Append the second model's MSE values to this list.
+three_mse_values = list()
+two_hyp_mse = dict()
+three_hyp_mse = dict()
+
+
+for hp in hyper_params:
+    knn = KNeighborsRegressor(n_neighbors=hp, algorithm='brute')
+    knn.fit(train_df[two_features], train_df['price'])
+    predictions = knn.predict(test_df[two_features])
+    mse = mean_squared_error(test_df['price'], predictions)
+    two_mse_values.append(mse)
+
+two_lowest_mse = two_mse_values[0]
+two_lowest_k = 1
+
+for k,mse in enumerate(two_mse_values):
+    if mse < two_lowest_mse:
+        two_lowest_mse = mse
+        two_lowest_k = k + 1
+    
+for hp in hyper_params:
+    knn = KNeighborsRegressor(n_neighbors=hp, algorithm='brute')
+    knn.fit(train_df[three_features], train_df['price'])
+    predictions = knn.predict(test_df[three_features])
+    mse = mean_squared_error(test_df['price'], predictions)
+    three_mse_values.append(mse)
+    
+three_lowest_mse = three_mse_values[0]
+three_lowest_k = 1
+
+for k,mse in enumerate(three_mse_values):
+    if mse < three_lowest_mse:
+        three_lowest_mse = mse
+        three_lowest_k = k + 1
+
+two_hyp_mse[two_lowest_k] = two_lowest_mse
+three_hyp_mse[three_lowest_k] = three_lowest_mse
+
+print(two_hyp_mse)
+print(three_hyp_mse)
+
