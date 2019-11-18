3c85944325b820900387b995ebd706a2026a0c60
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2019-11-18 23:23:20 +0100

    First commit DB

--- Working with data sources/mission1.1_introduction_SQL.py
+++ Working with data sources/mission1.1_introduction_SQL.py
@@ -19,3 +19,21 @@ where ShareWomen >= 0.5 and Median > 50000
 select Major, Median,  Unemployed from recent_grads
 where Median >= 10000 OR Unemployed <= 1000 
 limit 20
+
+# Grouping Operators With Parentheses
+select Major, Major_category, ShareWomen, Unemployment_rate from recent_grads
+where (Major_category = 'Engineering') and (ShareWomen > 0.5 or Unemployment_rate <0.051)
+
+# Ordering results using order by
+
+select Major, ShareWomen, Unemployment_rate from recent_grads
+where  ShareWomen >0.3 and Unemployment_rate <0.1
+order by ShareWomen desc
+
+# Practice writing a Query
+
+`Write a query that returns the Engineering or Physical Sciences majors in ascending order of unemployment rates`
+
+select Major_category, Major, Unemployment_rate from recent_grads
+where Major_category IN ('Engineering','Physical Sciences') 
+order by Unemployment_rate asc
